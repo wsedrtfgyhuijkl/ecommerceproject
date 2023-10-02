063cd58127cf4543c7a4a640b731433ba1c570c5
Vikas Choudhary <95460966+vikas-sd@users.noreply.github.com>
2023-10-02 17:18:03 +0530

    apiroute

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/product', [ProductController::class]);
+Route::apiResource('/review', [ReviewController::class]);
+
+// Route::prefix('products')->group(function(){
+// // Route::apiResource('')
+// });
+
