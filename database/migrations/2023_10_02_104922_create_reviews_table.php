a72d79f7095b882a712b526066dc0b4033c394fd
Vikas Choudhary <95460966+vikas-sd@users.noreply.github.com>
2023-10-02 17:36:11 +0530

    migration created for product and review

--- database/migrations/2023_10_02_104922_create_reviews_table.php
+++ database/migrations/2023_10_02_104922_create_reviews_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('/product', [ProductController::class]);
-Route::apiResource('/review', [ReviewController::class]);
+Route::apiResource('product', ProductController::class);
+Route::apiResource('review', ReviewController::class);
 
-// Route::prefix('products')->group(function(){
-// // Route::apiResource('')
-// });
+
+
 
