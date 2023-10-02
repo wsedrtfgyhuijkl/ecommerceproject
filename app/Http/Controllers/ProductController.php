d79e3b128afaed36df7cbe428d680b01cea2a4db
Vikas Choudhary <95460966+vikas-sd@users.noreply.github.com>
2023-10-02 18:54:19 +0530

    product resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\Product\ProductResource;
 
 class ProductController extends Controller
 {
@@ -13,7 +14,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::get();
+        // $product=compact('product');
+        // return response()->json([
+        //     "data"=>$product
+        // ]);
+        
     }
 
     /**
@@ -37,7 +43,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        // return $product;
+        return new ProductResource($product);
     }
 
     /**
